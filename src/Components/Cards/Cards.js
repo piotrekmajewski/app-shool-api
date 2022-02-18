88407bcd9f2a4d5769e8cf9e7028577989bd58eb
Piotr Majewski <piotrekmedia@gmail.com>
2022-02-18 11:54:46 +0100

    axios install and API test

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card/Card';
 import styles from './Cards.module.css';
 
-class Cards extends Component {
-    render() {
-        return (
-            <div className={styles.Cards}>
-                {this.props.cards.map(card => <Card key={card.id}{...card} />)}
-            </div>
-        );
-    }
+const Cards = () => {
+    fetch('http://universities.hipolabs.com/search?country=Poland')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+    return (
+        <div className={styles.Cards}>
+            {/* {this.props.cards.map(card => <Card key={card.id}{...card} />)} */}
+        </div>
+    );
 }
 
 export default Cards;
